114531850eb165ee301d45aa9e561c4a018371a2
dahua <970263611@qq.com>
2022-04-05 21:43:30 +0800

    优化手机端展示

--- src/main/java/com/dahuaboke/blog/BlogApplication.java
+++ src/main/java/com/dahuaboke/blog/BlogApplication.java
@@ -3,11 +3,12 @@ package com.dahuaboke.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
 }
